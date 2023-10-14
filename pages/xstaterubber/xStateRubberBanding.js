687a5844e4bd02aabb03a67ca3c46d69bd6522f5
Rémi Bastide <Remi.Bastide@irit.fr>
2023-10-14 18:07:32 +0200

    Commentaires sur rubber

--- pages/xstaterubber/xStateRubberBanding.js
+++ pages/xstaterubber/xStateRubberBanding.js
@@ -5,15 +5,18 @@ inspect({
   iframe: () => document.querySelector('iframe[data-xstate]')
 });
 
+// L'endroit où on va dessiner
 const stage = new Konva.Stage({
   container: "container",
   width: '400',
   height: 400,
 });
 
+// Une couche de dessin, il peut y en avoir plusieurs
 const layer = new Konva.Layer();
 stage.add(layer);
 
+// La ligne en cours de dessin
 let rubber;
 
 const rubberBandingMachine = createMachine(
@@ -45,20 +48,24 @@ const rubberBandingMachine = createMachine(
   },
   {
     actions: {
+        // Crée une ligne à la position du clic, les deux points sont confondus
       createLine: (context, event) => {
         const pos = stage.getPointerPosition();
         rubber = new Konva.Line({
+          // Les points de la ligne sont stockés comme un tableau de coordonnées x,y
           points: [pos.x, pos.y, pos.x, pos.y],
           stroke: "red",
           strokeWidth: 2,
         });
         layer.add(rubber);
       },
+      // Modifie le dernier point de la ligne en cours de dessin
       setLastPoint: (context, event) => {
         const pos = stage.getPointerPosition();
         rubber.points([rubber.points()[0], rubber.points()[1], pos.x, pos.y]);
         layer.batchDraw();
       },
+      // Sauvegarde la ligne
       saveLine: (context, event) => {
         // Save the line somewhere
       },
@@ -66,6 +73,7 @@ const rubberBandingMachine = createMachine(
   }
 );
 
+// On démarre la machine
 const rubberBandingService = 
 interpret(rubberBandingMachine, { devTools: true })
   .onTransition((state) => {
@@ -73,6 +81,7 @@ interpret(rubberBandingMachine, { devTools: true })
   })
   .start();
 
+// On transmet les événements souris à la machine
 stage.on("click", () => {
   rubberBandingService.send("MOUSECLICK");
 });
